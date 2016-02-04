5fe91d0735ee32f75472cbe1e721e9393a6e1086
Derry Shribman <derry@hola.org>
2016-02-04 20:34:02 +0200

    Update input_correctness.js

--- input_correctness.js
+++ input_correctness.js
@@ -1,10 +1,10 @@
 var assert = require('assert');
-exprots.substring_search_validate = function(substrings, text){
+exprots.valid_input = function(substrings, text){
     assert(Array.isArray(substrings));
     assert(substrings.length<=100);
     substrings.forEach(s=>{
         assert(typeof s=='string');
-        assert(s.length<=100);
+        assert(s.length>0 && s.length<=100);
         assert(/^[\x20-\x7f]*$/.test(s));
     });
     assert(typeof text=='string');
