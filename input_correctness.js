a5e81016407801d47f8f4425914836ef63521b2e
Derry Shribman <derry@hola.org>
2016-02-04 20:28:00 +0200

    Create input_correctness.js

---
+++ input_correctness.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+exprots.substring_search_validate = function(substrings, text){
+    assert(Array.isArray(substrings));
+    assert(substrings.length<=100);
+    substrings.forEach(s=>{
+        assert(typeof s=='string');
+        assert(/^[\x20-\x7f]*$/.test(s));
+    });
+    assert(typeof text=='string');
+    assert(/^[\x20-\x7f]*$/.test(text));
+};
