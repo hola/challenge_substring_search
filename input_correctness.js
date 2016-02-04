abbd3f953fedcfb3b1e2f5e6e6bb2e9a1a198c94
Derry Shribman <derry@hola.org>
2016-02-04 20:35:58 +0200

    Update input_correctness.js

--- input_correctness.js
+++ input_correctness.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
 exprots.valid_input = function(substrings, text){
+    assert(typeof text=='string');
+    assert(/^[\x20-\x7f]*$/.test(text));
     assert(Array.isArray(substrings));
     assert(substrings.length<=100);
+    var dup = {};
     substrings.forEach(s=>{
         assert(typeof s=='string');
         assert(s.length>0 && s.length<=100);
         assert(/^[\x20-\x7f]*$/.test(s));
+        assert(!dup[s]);
+        dup[s] = true;
     });
-    assert(typeof text=='string');
-    assert(/^[\x20-\x7f]*$/.test(text));
 };
